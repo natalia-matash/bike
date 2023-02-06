3530dc94ecd4518f1f0c0c4c66fc81a1fe82f7c5
Natalia Matash <natalimatash@gmail.com>
2023-02-06 19:26:48 +0200

    Fixed scroll in menu and changed form behaviour

---
+++ src/script.js
@@ -0,0 +1,12 @@
+window.addEventListener('hashchange', () => {
+  if (window.location.hash === '#menu') {
+    document.body.classList.add('page__body--with-menu');
+  } else {
+    document.body.classList.remove('page__body--with-menu');
+  }
+});
+
+document.getElementById('form').addEventListener('submit', (event) => {
+  event.preventDefault();
+  document.getElementById('form').reset();
+});
